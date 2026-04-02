e069ed3b74865006f2a4055be5f4fb866f1ddd0d
Farshana <farshana.marketeq@gmail.com>
2026-04-02 14:13:41 -0700

    ci: add GitHub Actions CI pipeline (lint, test with coverage)

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+const js = require('@eslint/js');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'commonjs',
+      globals: {
+        require: 'readonly',
+        module: 'readonly',
+        exports: 'readonly',
+        process: 'readonly',
+        console: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+    },
+  },
+];
